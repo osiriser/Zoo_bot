9c5f879a36f77513e9769afa7f2826bcf5f037c7
osiriser <radomir2012b@gmail.com>
2024-10-21 15:26:42 +0500

    ref-code

---
+++ pages/scripts/referral-code.js
@@ -0,0 +1,11 @@
+let WebApp = window.Telegram.WebApp;
+
+const params = new URLSearchParams(window.Telegram.WebApp.initData);
+
+const userData = Object.fromEntries(params);
+
+if (userData.user) {
+    userData.user = JSON.parse(userData.user);
+    const refcode = `https://t.me/ClothesShopX_bot?start=${userData.user.id}`;
+    document.getElementById("referralCode").value = refcode;
+}
