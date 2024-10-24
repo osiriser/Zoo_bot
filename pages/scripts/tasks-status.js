5a0409dd000dc18db67d9b9dd2b55edd15e88c3d
osiriser <radomir2012b@gmail.com>
2024-10-25 00:19:22 +0500

    tasks7

--- pages/scripts/tasks-status.js
+++ pages/scripts/tasks-status.js
@@ -9,9 +9,27 @@ window.onload = function() {
             if (data[taskId] === true) {
                 const taskButton = document.getElementById(taskId);
                 taskButton.classList.add('completed');
-                taskButton.querySelector('a').style.pointerEvents = 'none'; // Делаем ссылку неактивной
+                taskButton.querySelector('a').style.pointerEvents = 'none';
+                taskButton.disabled = true;
             }
         });
     })
     .catch(error => console.error('Ошибка загрузки состояния задач:', error));
+};
+
+
+
+let points = 0;
+let savedPoints = 0; // Очки, которые были подгружены из базы данных
+
+
+window.onload = function() {
+    fetch(`/api/get-user-points?user_id=${userId}`)
+        .then(response => response.json())
+        .then(data => {
+            points = data.points; // Инициализируем очки из базы данных
+            savedPoints = points;
+            document.getElementById("points").innerText = points;
+        })
+        .catch(error => console.error('Ошибка загрузки данных:', error));
 };
