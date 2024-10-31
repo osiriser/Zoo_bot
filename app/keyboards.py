4ad0df48e4da33b5d4f843eb788ad8ba0c2ba238
osiriser <radomir2012b@gmail.com>
2024-10-31 15:58:30 +0500

    Button to add product6

--- app/keyboards.py
+++ app/keyboards.py
@@ -9,14 +9,9 @@ async def start_keyboard():
 
 
 async def admin_keyboard():
-    button_add_category = InlineKeyboardButton("Add a category", callback_data="add_category")
-    button_add_subcategory = InlineKeyboardButton("Add a subcategory", callback_data="add_subcategory")
-    button_add_product = InlineKeyboardButton("Add a product", callback_data="add_product")
 
-    # Создание клавиатуры с использованием параметра inline_keyboard
-    admin_menu = InlineKeyboardMarkup(inline_keyboard=[
-        [button_add_category],
-        [button_add_subcategory],
-        [button_add_product]
-    ])
-    return admin_menu
+    markup = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="Add a category", callback_data="add_category"),
+         InlineKeyboardButton(text="Add a subcategory", callback_data="add_subcategory")],
+        [InlineKeyboardButton(text="Add a product", callback_data="add_product")]])
+    return markup
