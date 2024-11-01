01b2694042f7660d849e4e75fc3e8f857bedda99
osiriser <radomir2012b@gmail.com>
2024-11-01 23:11:04 +0500

    button add subcategory

--- app/handlers.py
+++ app/handlers.py
@@ -49,13 +49,13 @@ async def command_start_handler(message: Message, command: CommandObject):
 @router.callback_query(F.data == 'add_category')
 async def add_category_button(callback: CallbackQuery, state: FSMContext):
     await state.set_state(AddCategoryStates.waiting_for_name)
-    await callback.message.edit_text(text="Please send a category name")
+    await callback.message.answer(text="Please send a category name")
 
 @router.message(AddCategoryStates.waiting_for_name)
 async def process_waiting_for_name(message: Message, state: FSMContext):
     await state.update_data(waiting_for_name=message.text)
     await state.set_state(AddCategoryStates.waiting_for_photo)
-    await message.reply(text="Please send a category photo")
+    await message.reply(text="Please send a category photo as file")
 
 @router.message(AddCategoryStates.waiting_for_photo)
 async def process_waiting_for_photo(message: Message, state: FSMContext):
@@ -74,5 +74,46 @@ async def process_waiting_for_photo(message: Message, state: FSMContext):
     await message.bot.download_file(file.file_path, file_path)
     await state.update_data(image_path=file_path)
     await db_commands.add_category(name_category, file_path_short)
+    await message.reply("Success!")
+    await state.clear()
+
+
+
+@router.callback_query(F.data == 'add_subcategory')
+async def add_subcategory_button(callback: CallbackQuery, state: FSMContext):
+    await state.set_state(AddSubcategoryStates.waiting_for_category_id)
+    await callback.message.answer(text="Please choose a category and send number")
+    categories = await db_commands.get_categories_without_path()
+    await callback.message.answer(text=f"{categories}")
+
+@router.message(AddSubcategoryStates.waiting_for_category_id)
+async def process_waiting_for_category_id(message: Message, state: FSMContext):
+    await state.update_data(waiting_for_category_id=message.text)
+    await state.set_state(AddSubcategoryStates.waiting_for_name)
+    await message.reply(text="Please send a category name")
+
+@router.message(AddSubcategoryStates.waiting_for_name)
+async def process_waiting_for_name_subcategory(message: Message, state: FSMContext):
+    await state.update_data(waiting_for_name=message.text)
+    await state.set_state(AddSubcategoryStates.waiting_for_photo)
+    await message.reply(text="Please send a subcategory photo as file")
+
+@router.message(AddSubcategoryStates.waiting_for_photo)
+async def process_waiting_for_photo_subcategory(message: Message, state: FSMContext):
+    data = await state.get_data()
+    name_subcategory = data["waiting_for_name"]
+    category_id = data["waiting_for_category_id"]
+    photo = message.document  # Берем фото с наибольшим разрешением
+    file_id = photo.file_id
+    file = await message.bot.get_file(file_id)
+    
+    # Получаем исходное расширение файла
+    file_extension = file.file_path.split('.')[-1]
+    file_path = f"/home/developer/Zoo_bot/icons/{name_subcategory}.{file_extension}"  # Путь с расширением
+    file_path_short = f"icons/{name_subcategory}.{file_extension}"
+    
+    await message.bot.download_file(file.file_path, file_path)
+    await state.update_data(image_path=file_path)
+    await db_commands.add_subcategory(name_subcategory, file_path_short, int(category_id))
     await message.reply("Success!")
     await state.clear()
