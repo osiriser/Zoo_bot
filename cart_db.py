426916491f5753083cb333347aa1484f2dbf3300
osiriser <radomir2012b@gmail.com>
2024-11-11 09:52:05 +0500

    add cart

---
+++ cart_db.py
@@ -0,0 +1,26 @@
+import asyncpg
+import asyncio
+
+async def create_cart():
+    conn = await asyncpg.connect(database="users",
+                                 user="postgres",
+                                 host="localhost",
+                                 password="kuroishi31!",
+                                 port=5432)
+
+    await conn.execute("""
+                            DROP TABLE IF EXISTS cart;
+                            CREATE TABLE cart (
+                            id SERIAL PRIMARY KEY,
+                            user_id INTEGER NOT NULL,
+                            product_id INTEGER NOT NULL,
+                            product_price INTEGER NOT NULL,
+                            product_name VARCHAR(255) NOT NULL,
+                            product_image VARCHAR(255),
+                            quantity INTEGER DEFAULT 1,
+                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                            );
+                        """)
+
+    await conn.close()
+asyncio.run(create_cart())
